4b0a6c14b19da7212b4790bc8989a47a63c9c83b
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-13 22:55:13 -0300

    feat: initial project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "./assets/css/reset.css";
+import "./assets/css/colors.css";
+import "./assets/css/global.css";
+
+import { App } from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
