d0da06a4716e528541e7ac73f76d0a3dc57a1d80
rafaelvieiracosta <rvc.vieira@outlook.com>
2025-03-14 23:57:11 -0300

    feat: init create, delete and completed task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header/Header.tsx";
 
+import { Task } from "./interfaces.tsx";
+
 export function App() {
+  const [tasks, setTasks] = useState<Task[]>;
+
+  function createTask(newTask: Task) {
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(taskID: number) {
+    const remainingTasks = tasks.filter((task: Task) => {
+      return task.id !== taskID;
+    });
+
+    setTasks(remainingTasks);
+  }
+
   return (
     <>
-      <Header />
+      <Header onCreateTask={createTask} />
       <h1>todo list</h1>
     </>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,32 @@ import styles from "./Header.module.css";
 
 import { IconLogo } from "../Icons/IconLogo";
 import { IconPlus } from "../Icons/IconPlus";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-export function Header() {
-  const isNewTaskEmpty = false;
+import { Task } from "../../interfaces.tsx";
+
+interface HeaderProps {
+  onCreateTask: (task: Task) => void;
+}
+
+export function Header({ onCreateTask }: HeaderProps) {
+  const [newTaskText, setNewTaskText] = useState("");
+
+  function handleNewTaskTextChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    onCreateTask({
+      id: new Date().getTime() + Math.random(),
+      text: newTaskText,
+      isCompleted: false,
+    });
+  }
+
+  const isNewTaskTextEmpty = newTaskText.trim() === "";
   return (
     <header className={styles.header}>
       <div className={styles.header__logo}>
@@ -12,16 +35,18 @@ export function Header() {
       </div>
 
       <div className={styles["header__form-wrapper"]}>
-        <form className={styles.header__form}>
+        <form onSubmit={handleCreateNewTask} className={styles.header__form}>
           <input
             type="text"
+            value={newTaskText}
+            onChange={handleNewTaskTextChange}
             placeholder="Adicione uma nova tarefa"
             className={styles.header__form__input}
           />
 
           <button
             type="submit"
-            disabled={isNewTaskEmpty}
+            disabled={isNewTaskTextEmpty}
             className={styles.header__form__button}
           >
             Criar <IconPlus />
